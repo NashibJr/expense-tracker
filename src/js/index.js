3ee9d6314488315813e3c84561285c5a71ec6949
NashibJr <nashibjr@gmail.com>
2025-03-06 19:51:03 +0300

    --amend

--- src/js/index.js
+++ src/js/index.js
@@ -28,5 +28,46 @@ console.log(expenses, ">>>");
     inputs.forEach((input) => {
       input.value = "";
     });
+    window.location.reload();
   });
+
+  // Functionality that renders the expenses.
+  const tbody = document.querySelector("table tbody");
+
+  expenses.forEach((expense) => {
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+    const btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.onclick = () => {
+      const indexToBeDeleted = expenses?.findIndex(
+        (expense_) => expense_.id === expense.id
+      );
+      expenses.splice(indexToBeDeleted, 1);
+
+      alert(`${expense.description} is successfully deleted!!`);
+      localStorage.setItem("expenses", JSON.stringify(expenses));
+      window.location.reload();
+    };
+
+    cell.appendChild(btn);
+    row.innerHTML = `
+    <td>${expense.date}</td>
+    <td>${expense.description}</td>
+    <td>$${expense.amount}</td>
+    `;
+
+    row.appendChild(cell);
+    tbody.appendChild(row);
+  });
+  document.getElementById("totalExp").innerHTML =
+    "$" +
+    expenses
+      ?.map((expense) => Number(expense.amount)) //[200, 10, 15]
+      ?.reduce((prevValue, currentValue) => prevValue + currentValue);
+
+  document.getElementById("balance").innerHTML = `$${
+    Number(-document.getElementById("totalExp").innerHTML.slice(1)) +
+    Number(document.getElementById("income").innerHTML.slice(1))
+  }`;
 })();
